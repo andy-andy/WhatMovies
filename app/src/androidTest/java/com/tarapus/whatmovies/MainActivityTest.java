787dd96fcd54ecab7c55c6497a7d25581bf7f8ec
Andrey <andrey198318@gmail.com>
2016-03-24 23:06:10 -0500

    Test framework. Changes to the main activity name

---
+++ app/src/androidTest/java/com/tarapus/whatmovies/MainActivityTest.java
@@ -0,0 +1,31 @@
+package com.tarapus.whatmovies;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import com.tarapus.whatmovies.activities.MainActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Test
+    public void isRetryButtonClickable() {
+        onView(withId(R.id.retry_button)).check(matches(isClickable()));
+    }
+}
